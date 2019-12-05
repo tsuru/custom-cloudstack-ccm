d2de54c403121dc539fdf0c370f7a0ea42047583
Cezar Sa Espinola <cezarsa@gmail.com>
2019-12-05 11:25:16 -0300

    Use external ip index instead of hardcoded index for LB nic

---
+++ cloudstack/helpers_test.go
@@ -0,0 +1,41 @@
+package cloudstack
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	cloudstackFake "github.com/tsuru/custom-cloudstack-ccm/cloudstack/fake"
+	"github.com/xanzy/go-cloudstack/cloudstack"
+)
+
+func Test_newCloudstackManager(t *testing.T) {
+	srv := cloudstackFake.NewCloudstackServer()
+	defer srv.Close()
+	csCli := cloudstack.NewAsyncClient(srv.URL, "a", "b", true)
+	manager, err := newCloudstackManager(csCli)
+	require.NoError(t, err)
+	assert.NotNil(t, manager.client)
+	assert.NotNil(t, manager.cache)
+}
+
+func Test_cloudstackManager_virtualMachineByName(t *testing.T) {
+	srv := cloudstackFake.NewCloudstackServer()
+	defer srv.Close()
+	csCli := cloudstack.NewAsyncClient(srv.URL, "a", "b", true)
+	manager, err := newCloudstackManager(csCli)
+	require.NoError(t, err)
+	vm1, err := manager.virtualMachineByName("n1", "proj1")
+	require.NoError(t, err)
+	assert.Equal(t, vm1.Name, "n1")
+	srv.HasCalls(t, []cloudstackFake.MockAPICall{
+		{Command: "listVirtualMachines", Params: url.Values{"name": []string{"n1"}, "projectid": []string{"proj1"}}},
+	})
+	for i := 0; i < 100; i++ {
+		vm2, err := manager.virtualMachineByName("n1", "proj1")
+		require.NoError(t, err)
+		assert.Equal(t, vm1, vm2)
+	}
+	assert.Less(t, len(srv.Calls), 100)
+}
